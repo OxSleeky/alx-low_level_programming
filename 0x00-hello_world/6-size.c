92f6b8df0340c61a91b3231d0b303f82e8cea864
OxSleeky <aliusaliu1@gmail.com>
2023-06-19 06:49:17 -0700

    Size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Starting point for main
+ *
+ * Return: Will always return to 0
+ */
+int main(void)
+{
+	printf("Size of int is : %lu byte(s).\n", sizeof(int));
+	printf("Size of char is : %lu byte(s).\n", sizeof(char));
+	printf("Size of float is : %lu byte(s).\n", sizeof(float));
+	printf("Size of long int is : %lu byte(s).\n", sizeof(long int));
+	return (0);
---
+++ 0x00-hello_world/size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Size of int is : %lu byte(s).\n", sizeof(int));
+	printf("Size of char is : %lu byte(s).\n", sizeof(char));
+	printf("Size of float is : %lu byte(s).\n", sizeof(float));
+	printf("Size of long int is : %lu byte(s).\n", sizeof(long int));
+	return (0);
+}
